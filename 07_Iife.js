1af678751711de62bb2518402bd33b79b35bf41d
anjaanid <sujj4646@gmail.com>
2024-09-05 20:05:02 +0530

    Learned about IIFE in JS

--- 07_Iife.js
+++ 07_Iife.js
@@ -1,1 +1,14 @@
 // Immediately Invoked Function Expression (IIFE)
+
+// In IIFE we write the function inside a function and then execute the function immediately as the name suggests
+(function chai() {
+  // named iife
+//   console.log("DB CONNECTED");
+})();
+
+// an iife should end with semicolon or it will throw error, to write two iife we seperate them by semicolon
+
+((name) => {
+  // simple iife
+//   console.log(`DB CONNECTED TWO ${name}`);
+})("Sujal");
